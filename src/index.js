c9bf5aee0660c689a69659fca354563fb34f2dc0
pooh <cktjdgus45@naver.com>
2021-11-29 21:52:07 +0900

    practice redux reducer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore } from "redux";
 
 // ReactDOM.render(
 //   <React.StrictMode>
@@ -9,9 +10,23 @@ import App from './App';
 //   document.getElementById('root')
 // );
 const form = document.querySelector('form');
-const submit = form.querySelector('input[type="submit"]');
 
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
 
+const reducer = (state = [], action) => {
+  console.log(action);
+  switch (action.type) {
+    case ADD_TODO:
+      return [];
+    case DELETE_TODO:
+      return [];
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
 
 function addToDoList(todo) {
   const ul = document.querySelector('ul');
@@ -32,6 +47,7 @@ function handleSubmit(event) {
   const todo = input.value;
   addToDoList(todo);
   input.value = ""
+  store.dispatch({ type: ADD_TODO, text: todo })
 }
 
 form.addEventListener('submit', handleSubmit);
