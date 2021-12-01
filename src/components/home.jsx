98b56f8151102b8e43ac83c06fcc1053af3697ea
pooh <cktjdgus45@naver.com>
2021-12-01 14:02:06 +0900

    create redux and connect the store

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Home = (props) => {
+    const [text, setText] = useState('');
+    const onChange = (e) => {
+        setText(e.target.value);
+    }
+    const onSubmit = (e) => {
+        e.preventDefault();
+        setText('');
+    }
     return (
         <>
             <h1>리액트 리덕스 투듀 리스트</h1>
+            <form onSubmit={onSubmit}>
+                <input type="text" value={text} placeholder="할 일 적기" onChange={onChange} />
+                <button>ADD</button>
+            </form>
         </>
     )
 }
