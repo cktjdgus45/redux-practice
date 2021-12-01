f06abb180fa1e7df876ec08d4ac905b48a6b1ec0
pooh <cktjdgus45@naver.com>
2021-12-01 16:09:57 +0900

    getState,dispatch by react-redux func

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
 
-const Home = (props) => {
+const Home = ({ todos, addToDo, deleteToDo }) => {
+    console.log(todos)
     const [text, setText] = useState('');
     const onChange = (e) => {
         setText(e.target.value);
     }
     const onSubmit = (e) => {
         e.preventDefault();
+        addToDo(text);
         setText('');
     }
     return (
@@ -16,7 +19,19 @@ const Home = (props) => {
                 <input type="text" value={text} placeholder="할 일 적기" onChange={onChange} />
                 <button>ADD</button>
             </form>
+            <ul>{JSON.stringify(todos)}</ul>
         </>
     )
 }
-export default Home;
+function mapStateToProps(state) { //store.getState
+    return { todos: state }
+}
+function mapDispatchToProps(dispatch) { //store.dispatch
+    return {
+        // dispatching plain actions
+        addToDo: (text) => dispatch({ type: 'ADD', text }),
+        deleteToDo: (text) => dispatch({ type: 'DELETE', text }),
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
--- src/service/store.js
+++ src/service/store.js
@@ -3,14 +3,7 @@ import { createStore } from 'redux';
 const ADD = "ADD";
 const DELETE = "DELETE";
 
-const addToDo = (text) => {
-    store.dispatch({ type: ADD, text });
-}
-const deleteToDo = (id) => {
-    store.dispatch({ type: DELETE, id });
-}
-
-const reducer = (state = [], action) => {
+const reducer = (state = ['jell'], action) => {
     switch (action.type) {
         case ADD:
             return [{ text: action.text, id: Date.now() }, ...state];
@@ -22,6 +15,5 @@ const reducer = (state = [], action) => {
 }
 const store = createStore(reducer);
 
-store.subscribe();
 
 export default store;
